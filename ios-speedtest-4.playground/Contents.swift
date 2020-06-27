77b4a101deb83fc944061c404500a56f31711964
alzainalbannai <alzainalbannai@users.noreply.github.com>
2020-06-27 17:11:51 +0300

    done
    done

--- ios-speedtest-4.playground/Contents.swift
+++ ios-speedtest-4.playground/Contents.swift
@@ -5,22 +5,25 @@ import UIKit
 //: # 1️⃣ for-in loop
 
 //MARK: for- in loop خمس مرات باستخدام ال "iOS" قم بطباعه
-
-
+for i in 0..5{
+    
+ print("ios")
+    
+}
 
 
 //: # 2️⃣ function
 
 // MARK: هذي الداله تقوم بضرب رقمين و ترجع الناتج
 
-func Multiplication(  number1 : Int ,  number2 : Int)
+func Multiplication(  number1: Int,  number2 : Int)
 {
     
-    return number1 * number2
+    return (number1 * number2)
 }
 // MARK:   بما يناسب صيغه هذا الاستدعاء Multiplication قم بتعديل  الداله
 
-Multiplication(5,7)
+Multiplication(number1: 5,  number2 : 7)
 
 
 
@@ -32,3 +35,9 @@ Multiplication(5,7)
 
 // MARK:  الكائن  object مبنية علي هذي struct  قم بكتابة كود ال
 var student = Student(name: "Omar", age: 25 , hobbies: ["Coding", "drawing"])
+
+stuct Student{
+    var name : String
+    var age: Int
+    var hobbies: [String] }
+var student = Student(name: "Omar", age: 25, hobbies: ["Coding", "drawing"])
